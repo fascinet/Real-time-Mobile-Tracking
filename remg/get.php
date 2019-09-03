953f3e62800f246f80a1967688b8b6553d543d72
saisrirammortha <34328635+saisrirammortha@users.noreply.github.com>
2019-09-03 16:30:46 +0530

    Add files via upload
    seperate extensions.
    Need to integrate

---
+++ remg/get.php
@@ -0,0 +1,12 @@
+<?php
+
+$lat=(isset($_GET['lat']))?$_GET['lat']:'';
+$long=(isset($_GET['long']))?$_GET['long']:'';
+
+echo("GM\n");
+echo("Lat =".$lat);
+echo("Long=".$long);
+
+//do whatever you want
+//
+//?>
